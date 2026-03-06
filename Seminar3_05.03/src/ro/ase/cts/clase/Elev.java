f1a62b4215dc9499b81b7d06ce2c99a43cded6e2
Tabuncic Valeria <tabuncicvaleria851@gmail.com>
2026-03-06 19:48:37 +0200

    Seminar 3- 05.03

---
+++ Seminar3_05.03/src/ro/ase/cts/clase/Elev.java
@@ -0,0 +1,58 @@
+package ro.ase.cts.clase;
+
+import java.util.Arrays;
+
+public class Elev extends Aplicant{
+	private int clasa;
+	private String tutore;
+	private static int sumaFinantare=30;
+
+	public static void setSumaFinantare(int sumaFinantare)
+	{
+		Elev.sumaFinantare=sumaFinantare;
+	}
+	public int getClasa()
+	{
+		return clasa;
+	}
+	public void setClasa(int i)
+	{
+		this.clasa = i;
+	}
+	public String getTutore()
+	{
+		return tutore;
+	}
+	public void setTutore(String tutore)
+	{
+		this.tutore = tutore;
+	}
+
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder("Elev{");
+		sb.append(super.toString());
+		sb.append("clasa=").append(clasa);
+		sb.append(", tutore='").append(tutore).append('\'');
+		sb.append('}');
+		return sb.toString();
+	}
+
+	public Elev() {
+		super();
+	}
+	
+	public Elev(String nume, String prenume, int varsta, int punctaj,
+			int nrProiecte, String[] denumireProiect, int clasa, String tutore) {
+		super(nume,prenume,varsta,punctaj,nrProiecte,denumireProiect);
+		this.clasa = clasa;
+		this.tutore = tutore;
+	}
+
+	public void afisareFinantare() {
+
+		System.out.println("Elevul " + getNume() + " " + getPrenume() + " primeste" + Elev.sumaFinantare + " Euro/zi in proiect.");
+	}
+	
+}
---
+++ Seminar3_05.03/src/ro/ase/cts/clase/Student.java
@@ -0,0 +1,48 @@
+package ro.ase.cts.clase;
+
+import java.util.Arrays;
+
+public class Student extends Aplicant {
+	protected String facultate;
+	protected int anStudii;
+	private static int sumaFinantare=20;
+	
+
+	public void setFacultate(String facultate)
+	{
+		this.facultate = facultate;
+	}
+
+	public void setAnStudii(int anStudii)
+	{
+		this.anStudii = anStudii;
+	}
+
+
+	public Student() {
+		super();
+		
+	}
+	
+	public Student(String nume, String prenume, int varsta, int punctaj, int nr_proiecte, String[] denumireProiect, String facultate, int anStudii) {
+		super(nume,prenume,varsta,punctaj,nr_proiecte,denumireProiect);
+		this.facultate = facultate;
+		this.anStudii = anStudii;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder("Student{");
+		sb.append(super.toString());
+		sb.append("anStudii=").append(anStudii);
+		sb.append(", facultate='").append(facultate).append('\'');
+		sb.append('}');
+		return sb.toString();
+	}
+
+	public void afisareFinantare() {
+
+		System.out.println("Studentul " + getNume() + " " + getPrenume() + " primeste" + Student.sumaFinantare + " Euro/zi in proiect.");
+	}
+	
+}
