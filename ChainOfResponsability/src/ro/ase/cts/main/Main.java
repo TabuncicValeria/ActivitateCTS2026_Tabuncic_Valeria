2feaf3c6609e62c7953b580fc47adcf5b6b700fe
Tabuncic Valeria <tabuncicvaleria851@gmail.com>
2026-05-06 11:25:42 +0300

    Seminar 11 Extra - 05.05

---
+++ ChainOfResponsability/src/ro/ase/cts/main/Main.java
@@ -0,0 +1,20 @@
+package ro.ase.cts.main;
+
+import ro.ase.cts.clase.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Chain Of Responsability");
+        CalatorieHandler calatorieAutobuz = new CalatorieAutobuz();
+        CalatorieHandler calatorieTroleibuz = new CalatorieTroleibuz();
+        CalatorieHandler calatorieTramvai = new CalatorieTramvai();
+        CalatorieHandler calatorieMetrou = new CalatorieMetrou();
+
+        calatorieAutobuz.setCalatorieHandler(calatorieTroleibuz);
+        calatorieTroleibuz.setCalatorieHandler(calatorieTramvai);
+        calatorieTramvai.setCalatorieHandler(calatorieMetrou);
+
+        System.out.println(calatorieAutobuz.recomandaCalatorie(7));
+    }
+}
